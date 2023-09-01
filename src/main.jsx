3ddc94a4a8c3cb9b25498b29858b2551395d43b2
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-08-31 20:37:05 -0500

    first commit to production

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-//import {  } from "styled-components";
 import { createTheme, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
