68995a130dada062a01d7af67476d1a500c160ab
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-04 17:29:16 -0500

    port

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://project-back-vqbb-dev.fl0.io:8080/api",
+    baseURL: "https://project-back-vqbb-dev.fl0.io/api",
     withCredentials: true
 })
 export default instance
