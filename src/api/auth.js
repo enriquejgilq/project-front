355ac092fcb63dbedef7b2e38273a46eb9fbb46a
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-06 12:47:40 -0500

    fix1.0.21

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,4 +5,11 @@ export const registerRequest = user => axios.post(`/register`, user)
 
 export const loginRequest = user => axios.post(`/login`, user)
 
-export const verifyToken = token => axios.get(`/verifyToken`, { token: token })
+//export const verifyToken = token => axios.get(`/verifyToken`, { token: token })
+export const verifyToken = (token) => {
+    return axios.get("/verifyToken", {
+        headers: {
+            token: `${token}`,
+        },
+    });
+};
