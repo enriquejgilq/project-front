aeacb3a5605ad6a4ed179aa7cccbf8b2e0b09eb1
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-06 10:27:02 -0500

    fix1.0.13

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
+const getCookie = name => {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+};
+
+const myCookieValue = getCookie('token');
+
 const instance = axios.create({
     baseURL: "https://project-back-vqbb-dev.fl0.io/api",
     withCredentials: true,
@@ -8,6 +21,11 @@ const instance = axios.create({
     },
 });
 
-
+if (myCookieValue !== null) {
+    instance.defaults.headers.common["Cookie"] = `token=${myCookieValue}`;
+    console.log('Cookie value1:', myCookieValue);
+} else {
+    console.log('Cookie not found');
+}
 
 export default instance;
