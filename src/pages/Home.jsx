d749c0fd36ae3bd3f05aa5d9d972994ccb2a281a
enriquejgilq <35854635+enriquejgilq@users.noreply.github.com>
2023-09-08 17:05:23 -0500

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,9 @@ import { useAuth } from "../context/AuthContext";
 import { useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import PetitionNotFound from "../pages/PetitionNotFound";
+import "atropos/css";
+import Atropos from "atropos/react";
+import styles from "./styles.module.css";
 
 const handleTo = (link) => {
   window.open(link, "_blank");
@@ -87,30 +90,48 @@ const Home = () => {
       <div className="w-full h-screen bg-gradient-to-bl from-green-900 via-gren-800 to-black   " />
       <div className="w-full h-screen absolute top-0 left-0 ">
         <div className="max-w-[700px] m-auto h-full w-full flex flex-col !justify-center lg:items-start lg:justify-start !items-center">
-          <h1 className="sm:text-5xl text-4xl font-bold text-black-800 text-center sm:text-center md:w-[300px] md:text-3xl text-white ">
-            Hola, soy {userName}
-          </h1>
-          <h2 className="flex sm:text-3xl text-2xl pt-4 text-black-800 text-white">
-            Yo soy
-            <TypeAnimation
-              sequence={sequence}
-              wrapper="span"
-              speed={50}
-              style={{ fontSize: "1em", paddingLeft: "5px", color: "white" }}
-              repeat={Infinity}
-            />
-          </h2>
-          <div className="flex justify-center space-x-5 pt-6 max-w-[200px] w-full">
-            {mergedLinks.map((social, index) => (
-              <div key={index}>
-                <social.icon
-                  onClick={() => handleTo(social.link)}
-                  size={20}
-                  className="cursor-pointer fill-white"
-                />
+          <Atropos
+            className={styles.styleAtropos}
+            activeOffset={40}
+            shadowScale={1.05}
+            shadow={true}
+            onEnter={() => console.log("Enter")}
+            onLeave={() => console.log("Leave")}
+            onRotate={(x, y) => console.log("Rotate", x, y)}
+          >
+            <div className={styles.card}>
+              <div>
+                <h1 className={styles.textprincipal}>Hola, soy {userName}</h1>
+                <div className={styles.textmachine}>
+                  <h2>
+                    <TypeAnimation
+                      sequence={sequence}
+                      wrapper="span"
+                      speed={50}
+                      style={{
+                        fontSize: "1em",
+                        paddingLeft: "5px",
+                        color: "white",
+                      }}
+                      repeat={Infinity}
+                    />
+                  </h2>
+                </div>
+
+                <div className={styles.link}>
+                  {mergedLinks.map((social, index) => (
+                    <div key={index}>
+                      <social.icon
+                        onClick={() => handleTo(social.link)}
+                        size={20}
+                        className="cursor-pointer fill-white"
+                      />
+                    </div>
+                  ))}
+                </div>
               </div>
-            ))}
-          </div>
+            </div>
+          </Atropos>
         </div>
       </div>
     </div>
